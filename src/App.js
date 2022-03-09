b3116f91f0b906747dd710fac966270097e8d493
kalpshah485 <kalpshah485@gmail.com>
2022-03-09 15:15:48 +0530

    feat: static to dynamic user data.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import UserList from './components/UserList/UserList';
 import './App.css';
+import { useEffect } from 'react';
+import { fetchUsers } from './redux/actions';
+import { useDispatch } from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [])
+  
   return (
     <>
-    <UserList />
+      <UserList />
     </>
   );
 }
